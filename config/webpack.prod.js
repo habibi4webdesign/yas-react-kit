f6b5bf22d87a58ae52769a484c5a81aab2b80352
yashar habibi <habibi4webdesign@gmail.com>
2022-08-08 14:05:06 +0200

    test

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -30,8 +30,8 @@ const prodConfig = {
   },
   resolve: {
     alias: {
-      react: path.resolve(__dirname, "./node_modules/react"),
-      "react-dom": path.resolve(__dirname, "./node_modules/react-dom"),
+      react: path.resolve(__dirname, "../node_modules/react"),
+      "react-dom": path.resolve(__dirname, "../node_modules/react-dom"),
     },
   },
   externals: {
