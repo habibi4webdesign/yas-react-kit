4e846b3a51b30b0f2045cbc9486c280f135efdd0
yashar habibi <habibi4webdesign@gmail.com>
2022-08-08 13:26:38 +0200

    test

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,4 +1,5 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
 const { merge } = require("webpack-merge");
 const commonConfig = require("./webpack.common");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
