42ea6f28af16e5b5643c81ecf7a4e635e4215edc
yashar habibi <habibi4webdesign@gmail.com>
2022-08-08 13:42:40 +0200

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import { Button } from "components";
 import React from "react";
-import { createRoot } from 'react-dom/client';
+import ReactDOM from "react-dom";
 
 const App = () => {
   return (
     <div><Button label="Log"></Button></div>
   )
 }
-
-const root = createRoot(document.getElementById('root') as HTMLElement);
+// @ts-ignore
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
