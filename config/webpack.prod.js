b8ac71d33ad8a6d80e44a9375c7595a85fc82e4e
yashar habibi <habibi4webdesign@gmail.com>
2022-08-08 13:25:50 +0200

    test

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -3,13 +3,17 @@ const { merge } = require("webpack-merge");
 const commonConfig = require("./webpack.common");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const pkg = require('../package.json');
 
 const prodConfig = {
   mode: "production",
   output: {
     filename: "index.js",
     publicPath: "/",
+    path: path.resolve(__dirname, "../dist"),
     clean: true,
+    library: pkg.name,
+    libraryTarget: "commonjs",
   },
   optimization: {
     minimize: true,
@@ -22,6 +26,27 @@ const prodConfig = {
         use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
     ],
+  },
+  resolve: {
+    alias: {
+      react: path.resolve(__dirname, "./node_modules/react"),
+      "react-dom": path.resolve(__dirname, "./node_modules/react-dom"),
+    },
+  },
+  externals: {
+    // Don't bundle react or react-dom
+    react: {
+      commonjs: "react",
+      commonjs2: "react",
+      amd: "React",
+      root: "React",
+    },
+    "react-dom": {
+      commonjs: "react-dom",
+      commonjs2: "react-dom",
+      amd: "ReactDOM",
+      root: "ReactDOM",
+    },
   },
 };
 
